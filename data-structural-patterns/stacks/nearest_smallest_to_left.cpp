93b8b2847bfc62c892ee2c2f02feb9fd5e8a89e5
nischaldutt <nischaldutt01@gmail.com>
2020-10-25 16:36:08 +0530

    adding nearest smallest to left problem

---
+++ data-structural-patterns/stacks/nearest_smallest_to_left.cpp
@@ -0,0 +1,50 @@
+// Find nearestSmallestToLeftrest smallest element to the left of current element.
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <stack>
+#define null nullptr
+
+using namespace std;
+
+void nearestSmallestToLeft(vector<int> arr, int n) {
+  vector<int> result;
+  stack<int> myStack;
+  for(int i = 0; i < n; i++) {
+    if(myStack.empty()) {
+      result.push_back(-1);
+    }
+    else if(!myStack.empty() && myStack.top() < arr[i]) {
+      result.push_back(myStack.top());
+    }
+    else if(!myStack.empty() && myStack.top() >= arr[i]) {
+      while(!myStack.empty() && myStack.top() >= arr[i]) {
+        myStack.pop();
+      }
+      if(myStack.empty()) {
+        result.push_back(-1);
+      }
+      else {
+        result.push_back(myStack.top());
+      }
+    }
+    myStack.push(arr[i]);
+  }
+
+  for(auto element: result) {
+    cout<<element<<" ";
+  }
+  return;
+}
+
+int main() {
+  int n, element;
+  cin>>n;
+  vector<int> arr;
+  while(n--) {
+    cin>>element;
+    arr.push_back(element);
+  }
+  nearestSmallestToLeft(arr, arr.size());
+  return 0;
+}
