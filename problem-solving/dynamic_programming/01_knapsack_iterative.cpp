6c9e7a02ce82c3b7c46fd8df2e8312550fa54df7
nischaldutt <nischaldutt01@gmail.com>
2023-01-02 06:48:54 +0530

    problem: iterative solution to 01 knapsack solution

---
+++ problem-solving/dynamic_programming/01_knapsack_iterative.cpp
@@ -0,0 +1,86 @@
+// 0/1 knapsack problem
+
+// input:
+// enter n: 4
+// enter weights: 1, 3, 4, 5
+// enter profits: 1, 4, 5, 7
+// enter weight of the knapsack: 7
+
+// output:
+// max profit: 9
+
+// time complexity: O(n*w) i.e. proportional to sizeof the matrix
+
+// iterative solution
+
+#include <bits/stdc++.h>
+
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <stack>
+
+#define null nullptr
+#define fori for (int i = 0; i < n; i++)
+#define forj for (int j = 0; j < w; j++)
+#define fork for (int k = 0; k < o; k++)
+
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef pair<int, pair<int, int>> ppi;
+
+int knapsack(vector<int> weightVector, vector<int> profitVector, int w, int n, int** matrix) {
+  for (int i = 1; i < n + 1; i++) {
+    for (int j = 1; j < w + 1; j++) {
+      if (weightVector[i - 1] <= w) {
+        matrix[i][j] = max(profitVector[i - 1] + knapsack(weightVector, profitVector, w - weightVector[i - 1], i - 1, matrix),
+                           knapsack(weightVector, profitVector, w, i - 1, matrix));
+      } else if (weightVector[i - 1] > w) {
+        matrix[i][j] = knapsack(weightVector, profitVector, w, i - 1, matrix);
+      }
+    }
+  }
+
+  return matrix[n][w];
+}
+
+int main() {
+  int weight, profit, n, w;
+
+  cout << "enter n: ";
+  cin >> n;
+
+  vector<int> weightVector;
+  vector<int> profitVector;
+
+  cout << "enter weights: ";
+  fori {
+    cin >> weight;
+    weightVector.push_back(weight);
+  }
+
+  cout << "enter profits: ";
+  fori {
+    cin >> profit;
+    profitVector.push_back(profit);
+  }
+
+  cout << "enter the weight of knapsack: ";
+  cin >> w;
+
+  int** matrix;
+  matrix = new int*[n + 1];
+
+  for (int i = 0; i < n + 1; i++)
+    matrix[i] = new int[w + 1];
+
+  for (int i = 0; i < n + 1; i++)
+    for (int j = 0; j < w + 1; j++)
+      if (i == 0 || j == 0)
+        matrix[i][j] = 0;
+
+  int maxProfit = knapsack(weightVector, profitVector, w, n, matrix);
+
+  cout << "maximum profit: " << maxProfit << endl;
+}
